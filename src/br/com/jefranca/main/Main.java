41c84e657380129670981aa597d827514f15767b
JEFRANCA <jefranca42@gmail.com>
2017-10-29 20:15:10 -0200

    HOY
    hoy.v2

--- src/br/com/jefranca/main/Main.java
+++ src/br/com/jefranca/main/Main.java
@@ -3,8 +3,7 @@ package br.com.jefranca.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("iai seu babaca");
 	}
 
 }
